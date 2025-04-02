6b0fa7ae1457076547dfbd0cc5c8197851771bcb
joregesosa <joregesosa@gmail.com>
2025-04-02 18:58:18 -0400

    remove unused migration and authentication middleware files; update import path in sequelize.js

--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
-import config from "../config/index.js";
+import config from "#config/index.js";
 
 const { database, password, port, host, user, dialect } = config.db;
 
