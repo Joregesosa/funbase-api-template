85fcfdfa804c6227164b8aec660892efbbaa6b51
Jorge Sosa <joregesosa@gmail.com>
2025-05-13 12:40:03 -0400

    refactor Users module: enhance controller documentation, add changePassword function, and update router to include new endpoint

--- src/modules/Users/Controller.js
+++ src/modules/Users/Controller.js
@@ -1,14 +1,21 @@
+/**
+ * @module UsersController
+ * @description Controlador para gestionar las operaciones relacionadas con los usuarios.
+ */
 import { hash } from "bcrypt";
 import { Users } from "./Model.js"
 
 /**
- * @description Get all Userss
- * @param {Request} req
- * @param {Response} res
- * @param {NextFunction} next
+ * @description Obtiene todos los usuarios activos.
+ * @param {Request} req - Objeto de solicitud HTTP.
+ * @param {Response} res - Objeto de respuesta HTTP.
+ * @param {NextFunction} next - Función para pasar el control al siguiente middleware.
+ * @returns {Promise<void>} Responde con una lista de usuarios activos.
  */
 export const index = async (req, res, next) => {
   try {
+    //#swagger.tags = ['Users']
+    //#swagger.description = 'Obtiene todos los usuarios activos.'
     const users = await Users.findAll({
       where: { status: true },
       attributes: { exclude: ["password"] }
@@ -20,14 +27,18 @@ export const index = async (req, res, next) => {
 };
 
 /**
- * @description Get a single Users
- * @param {Request} req
- * @param {Response} res
- * @param {NextFunction} next
- * @returns {Promise<void>}
+ * @description Obtiene un usuario por su ID.
+ * @param {Request} req - Objeto de solicitud HTTP.
+ * @param {Number} req.params.id - ID del usuario a obtener.
+ * @param {Response} res - Objeto de respuesta HTTP.
+ * @param {NextFunction} next - Función para pasar el control al siguiente middleware.
+ * @returns {Promise<void>} Responde con los datos del usuario solicitado.
  */
 export const show = async (req, res, next) => {
   try {
+    //#swagger.tags = ['Users']
+    //#swagger.description = 'Obtiene un usuario por su ID.'
+
     const user = await Users.findByPk(req.params.id);
 
     if (!user) {
@@ -41,19 +52,19 @@ export const show = async (req, res, next) => {
 };
 
 /**
- * @description Create a new Users
- * @param {Request} req
- * @param {Response} res
- * @param {NextFunction} next
- * @returns {Promise<void>}
+ * @description Crea un nuevo usuario.
+ * @param {Request} req - Objeto de solicitud HTTP.
+ * @param {Response} res - Objeto de respuesta HTTP.
+ * @param {NextFunction} next - Función para pasar el control al siguiente middleware.
+ * @returns {Promise<void>} Responde con un mensaje de éxito si el usuario es creado correctamente.
  */
 export const store = async (req, res, next) => {
   try {
-    const requeredFields = ["name", "lastname", "email", "password"];
-    const missingFields = requeredFields.filter((field) => !req.body[field]);
-
-    if (missingFields.length > 0) {
-      throw { status: 400, message: `Missing fields: ${missingFields.join(", ")}` };
+    //#swagger.tags = ['Users']
+    //#swagger.description = 'Crea un nuevo usuario.'
+    const { name, lastname, email, password } = req.body;
+    if (!name || !lastname || !email || !password) {
+      throw { status: 400, message: "Missing fields" };
     }
 
     const existingUser = await Users.findOne({ where: { email: req.body.email } });
@@ -79,19 +90,29 @@ export const store = async (req, res, next) => {
 };
 
 /**
- * @description Update a Users
- * @param {Request} req
- * @param {Response} res
- * @param {NextFunction} next
- * @returns {Promise<void>}
+ * @description Actualiza los datos de un usuario existente.
+ * @param {Request} req - Objeto de solicitud HTTP.
+ * @param {Number} req.params.id - ID del usuario a actualizar. 
+ * @param {Response} res - Objeto de respuesta HTTP.
+ * @param {NextFunction} next - Función para pasar el control al siguiente middleware.
+ * @returns {Promise<void>} Responde con los datos actualizados del usuario.
  */
 export const update = async (req, res, next) => {
   try {
+    //#swagger.tags = ['Users']
+    //#swagger.description = 'Actualiza los datos de un usuario existente.'  
+    const { name, lastname, email, password } = req.body;
+
+    if (!name && !lastname && !email && !password) {
+      throw { status: 400, message: "No data to update" };
+    }
+
     const users = await Users.findByPk(req.params.id);
     if (!users) {
       throw { status: 404, message: "Users not found" };
     }
-    await Users.update(req.body);
+    await users.update(req.body);
+    await users.save();
     res.status(200).json(users);
   } catch (error) {
     next(error);
@@ -99,17 +120,20 @@ export const update = async (req, res, next) => {
 };
 
 /**
- * @description Delete a Users
- * @param {Request} req
- * @param {Response} res
- * @param {NextFunction} next
- * @returns {Promise<void>}
+ * @description Elimina (desactiva) un usuario por su ID. (Borrado lógico)
+ * @param {Request} req - Objeto de solicitud HTTP.
+ * @param {Number} req.params.id - ID del usuario a eliminar.
+ * @param {Response} res - Objeto de respuesta HTTP.
+ * @param {NextFunction} next - Función para pasar el control al siguiente middleware.
+ * @returns {Promise<void>} Responde con un estado 204 si la operación es exitosa.
  */
 export const destroy = async (req, res, next) => {
   try {
+    //#swagger.tags = ['Users']
+    //#swagger.description = 'Elimina (desactiva) un usuario por su ID. (Borrado lógico)'
     const user = await Users.findByPk(req.params.id);
     if (!user) {
-      throw { status: 404, message: "Users not found" };
+      throw { status: 404, message: "User not found" };
     }
     await user.update({ status: false });
     await user.save();
@@ -120,14 +144,17 @@ export const destroy = async (req, res, next) => {
 };
 
 /**
- * @description Restore a Users
- * @param {Request} req
- * @param {Response} res
- * @param {NextFunction} next
- * @returns {Promise<void>}
- */
+  * @description Restaura un usuario previamente desactivado.
+  * @param {Request} req - Objeto de solicitud HTTP.
+  * @param {Number} req.params.id - ID del usuario a restaurar.
+  * @param {Response} res - Objeto de respuesta HTTP.
+  * @param {NextFunction} next - Función para pasar el control al siguiente middleware.
+  * @returns {Promise<void>} Responde con los datos del usuario restaurado.
+  */
 export const restore = async (req, res, next) => {
   try {
+    //#swagger.tags = ['Users']
+    //#swagger.description = 'Restaura un usuario previamente desactivado.'
     const user = await Users.findByPk(req.params.id);
     if (!user) {
       throw { status: 404, message: "Users not found" };
@@ -141,19 +168,20 @@ export const restore = async (req, res, next) => {
 }
 
 /**
- * @description Get profile of the authenticated user
- * @param {Request} req
- * @param {Response} res
- * @param {NextFunction} next
- * @returns {Promise<void>}
+ * @description Obtiene el perfil del usuario autenticado.
+ * @param {Request} req - Objeto de solicitud HTTP.
+ * @param {Response} res - Objeto de respuesta HTTP.
+ * @param {NextFunction} next - Función para pasar el control al siguiente middleware.
+ * @returns {Promise<void>} Responde con los datos del perfil del usuario autenticado.
  */
 export const profile = async (req, res, next) => {
   try {
+    //#swagger.tags = ['Users']
     const user = await Users.findByPk(req.user.id);
     if (!user) {
       throw { status: 404, message: "Users not found" };
     }
-    
+
     res.status(200).json(user);
 
   } catch (error) {
@@ -161,4 +189,51 @@ export const profile = async (req, res, next) => {
   }
 }
 
-export default { index, show, store, update, destroy, restore, profile };
+/**
+ * Cambia la contraseña de un usuario autenticado.
+ *
+ * @async
+ * @function changePassword
+ * @param {Object} req - Objeto de solicitud HTTP.
+ * @param {Object} req.auth - Información de autenticación del usuario.
+ * @param {number} req.auth.id - ID del usuario autenticado.
+ * @param {Object} req.body - Cuerpo de la solicitud HTTP.
+ * @param {string} req.body.oldPassword - Contraseña actual del usuario.
+ * @param {string} req.body.newPassword - Nueva contraseña para el usuario.
+ * @param {Object} res - Objeto de respuesta HTTP.
+ * @param {Function} next - Función para pasar el control al siguiente middleware.
+ * @throws {Object} Si el usuario no es encontrado, lanza un error con estado 404.
+ * @throws {Object} Si la contraseña actual es inválida, lanza un error con estado 401.
+ * @returns {void} Responde con un mensaje de éxito si la contraseña se cambia correctamente.
+ */
+export const changePassword = async (req, res, next) => {
+  try {
+    //#swagger.tags = ['Users']
+    //#swagger.description = 'Cambia la contraseña de un usuario autenticado.'
+    const { id } = req.auth;
+    const { oldPassword, newPassword } = req.body;
+
+    const user = await Users.findByPk(id);
+    if (!user) {
+      throw { status: 404, message: "User not found" };
+    }
+
+    const validPassword = await compare(oldPassword, user.password);
+
+    if (!validPassword) {
+      throw { message: "Invalid email or password", status: 401 };
+    }
+
+    const hashedPassword = await hash(newPassword, 10);
+
+    await user.update({ password: hashedPassword });
+    await user.save();
+    res.status(200).json({ message: "Password changed successfully" });
+  } catch (error) {
+    next(error);
+  }
+
+}
+
+
+export default { index, show, store, update, destroy, restore, profile, changePassword };
--- src/modules/Users/Router.js
+++ src/modules/Users/Router.js
@@ -1,12 +1,33 @@
+/**
+ * Users Router Module
+ * 
+ * This module defines the routes for managing users in the application.
+ * It uses Express Router to handle HTTP requests and maps them to the appropriate controller functions.
+ * 
+ * Routes:
+ * - GET "/" - Fetch a list of all users. (Controller: index)
+ * - GET "/profile" - Fetch the profile of the currently authenticated user. (Controller: profile)
+ * - PATCH "/change-password" - Change the password of the currently authenticated user. (Controller: changePassword)
+ * - GET "/:id" - Fetch details of a specific user by ID. (Controller: show)
+ * - POST "/" - Create a new user. (Controller: store)
+ * - PUT "/:id" - Update an existing user by ID. (Controller: update)
+ * - DELETE "/:id" - Delete a user by ID. (Controller: destroy)
+ * - PATCH "/:id/restore" - Restore a previously deleted user by ID. (Controller: restore)
+ * 
+ * @module UsersRouter
+ * @requires express.Router
+ * @requires ./Controller.js
+ */
 import { Router } from "express";
-import { index, show, store, update, destroy, profile, restore } from "./Controller.js";
+import { index, show, store, update, destroy, profile, restore, changePassword } from "./Controller.js";
 
 export const usersRouter = Router();
 
-usersRouter.get("/", index);
-usersRouter.get("/:id", show);
-usersRouter.post("/", store);
-usersRouter.put("/:id", update);
-usersRouter.delete("/:id", destroy); 
-usersRouter.patch("/:id/restore", restore);
-usersRouter.get("/profile", profile); 
+usersRouter.get("/users/", index);
+usersRouter.get("/users/profile", profile); 
+usersRouter.patch("/users/change-password", changePassword); 
+usersRouter.get("/users/:id", show);
+usersRouter.post("/users/", store);
+usersRouter.put("/users/:id?", update);
+usersRouter.delete("/users/:id", destroy); 
+usersRouter.patch("/users/:id/restore", restore);
