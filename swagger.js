4f2f0151d23642020be2b8984fd51285198871d1
Jorge Sosa <joregesosa@gmail.com>
2025-06-16 16:36:37 -0400

    refactor: simplify host assignment in swagger configuration

--- swagger.js
+++ swagger.js
@@ -1,12 +1,13 @@
 import swaggerAutogen from 'swagger-autogen';
 import conf from '#config/index.js';
 import { routes } from './src/routes/routes.js';
+const host = conf.env === 'production' ? conf.host : `${conf.host}:${conf.port}`;
 const doc = {
     info: {
         title: conf.appName,
         description: conf.appDescription,
-    },
-    host: `${conf.host}:${conf.port}`,
+    }, 
+    host: host,
     securityDefinitions:{
         'authorization': {
             type: 'apiKey',
