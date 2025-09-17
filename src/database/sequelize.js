d51ed05f1e1134a511201cb0c62acd1ce3ef1a79
Jorge Sosa <joregesosa@gmail.com>
2025-09-17 14:45:32 -0400

    refactor: remove unused readline import from sequelize.js

--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -1,6 +1,5 @@
 import { Sequelize } from "sequelize";
 import config from "#config/index.js";
-import readline from "readline";
 
 const { database, password, port, host, user, dialect } = config.db;
 const aditional = {
